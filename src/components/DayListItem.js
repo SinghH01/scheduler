bf58175c01771f33ef15b574d6618c8c2f444de2
Harshbir Singh <singh.hveer1@gmail.com>
2022-05-23 20:53:29 -0600

    Create to function to return appropriate string

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,7 +2,18 @@ import React from "react";
 import "components/DayListItem.scss"
 import classNames from "classnames";
 
-export default function DayListItem(props) {
+export default function DayListItem(props) {  
+  // Function to return appropriate message based on how many spots are available
+  function formatSpots(spots) {    
+    if (spots === 0) {
+      return "no spots remaining";
+    }
+    if (spots === 1) {
+      return "1 spot remaining";
+    }
+    
+    return `${spots} spots remaining`;
+  }
 
   let dayClass = classNames(
     "day-list__item",
@@ -11,11 +22,11 @@ export default function DayListItem(props) {
       "day-list__item--full" : props.spots === 0
     }
   )
-  
+
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
