780f2b34bb26bb6826824047657780ee46ccd5fd
Harshbir Singh <singh.hveer1@gmail.com>
2022-06-03 07:39:32 -0600

    Fetch days data using API call

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,27 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "components/Application.scss";
 import Appointment from "./Appointment";
 import DayList from "./DayList";
+import axios from "axios";
 
 
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 const appointments = {
   "1": {
     id: 1,
@@ -63,7 +46,17 @@ const appointments = {
 
 
 export default function Application(props) {
-  const [day, setDay] = useState('Monday');
+  const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(()=> {
+    // Fetch days data using API 
+    axios.get("http://localhost:8001/api/days")
+    .then(response => {
+      setDays(response.data);
+    },[days])
+
+  },[])
 
   return (
     <main className="layout">
