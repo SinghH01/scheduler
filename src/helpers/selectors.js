edc714840e74965a644a5e8869dcb55b90d10125
Harshbir Singh <singh.hveer1@gmail.com>
2022-06-03 08:06:07 -0600

    Create function to return appointments for a specific day

---
+++ src/helpers/selectors.js
@@ -0,0 +1,20 @@
+//Function to return appointments for a specific day
+
+export function getAppointmentsForDay(state, day) {
+  
+  // Array to be returned
+  let appointmentArray = [];
+
+  for(const item of state.days) {
+    if(item.name === day) {
+
+      // Loop through appointments of specific day and push it into the array to be returned
+      for(const individualAppointment of item.appointments) {
+        appointmentArray.push(state.appointments[individualAppointment]);
+      }
+    }
+  }
+
+  return appointmentArray;
+
+}
