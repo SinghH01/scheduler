2be5beb7a237043cbb5a253b084d368097809201
Harshbir Singh <singh.hveer1@gmail.com>
2022-06-06 16:26:01 -0600

    Create function to return interviewer data

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,7 +1,7 @@
 //Function to return appointments for a specific day
 
 export function getAppointmentsForDay(state, day) {
-  
+
   // Array to be returned
   let appointmentArray = [];
 
@@ -17,4 +17,22 @@ export function getAppointmentsForDay(state, day) {
 
   return appointmentArray;
 
+}
+
+export function getInterview(state, interview){
+  let interviewData = {};
+  if(interview) {
+    interviewData = {
+      student: interview.student,
+      interviewer: state.interviewers[interview.interviewer] 
+    }
+    //console.log(interviewData)
+     return interviewData;
+
+     
+  } else {
+
+    return null;
+
+  }
 }
