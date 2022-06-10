4df4b972ae1b8ee25308fe2d6247a37b31685e33
Harshbir Singh <singh.hveer1@gmail.com>
2022-06-09 20:03:39 -0600

    Add console log statements to check the status of history state

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,31 +1,40 @@
-import React, { useState, useEffect } from "react";
+import { useState } from "react";
 
 //Function to switch between different modes of component
 export default function useVisualMode(initial) {
+
   const [mode, setMode] =  useState(initial);
   const [history, setHistory] = useState([initial]);
 
   //Set the mode to a new mode and save the previous mode in history state
   function transition (newMode, replace = false) {    
-    console.log("------->1"+ history);
+    
     let newValue = [...history, newMode];
-    setHistory(newValue);    
+    //setHistory(prev => ([...prev, newValue]));   
+    setHistory(newValue) 
     setMode(newMode);
 
     if(replace === true) {
       newValue.pop();
-      setHistory(newValue);
+      //setHistory(prev => ([...prev, newValue])); 
+      setHistory(newValue)
     }
+    console.log("====================")
+    console.log(history);
+    console.log("====================")
   }
 
   //Go back to previous mode
   function back(){
     if(history.length > 1) {
-      console.log("------->2"+ history);
+      
       history.pop();
       let newValue = history[history.length-1];
       setMode(newValue);
     }
+    console.log("========++++++============")
+    console.log(history);
+    console.log("====================")
   }
   return {mode, transition, back}
 }
