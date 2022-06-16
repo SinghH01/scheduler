7b98ceac31fc19c848fb6c2ce6cb54ba6d70689e
Harshbir Singh <singh.hveer1@gmail.com>
2022-06-16 01:13:24 -0600

    Import PropTypes

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "components/InterviewerList.scss"
 import InterviewerListItem from "./InterviewerListItem";
+import PropTypes from 'prop-types';
 
 
 
@@ -25,3 +26,11 @@ export default function InterviewerList(props) {
     </section>
   );
 }
+
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
+
+
+
