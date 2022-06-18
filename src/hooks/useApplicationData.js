2bc9ab6d9d9846abdaf147cb1bad822f97e13438
Harshbir Singh <singh.hveer1@gmail.com>
2022-06-18 11:17:37 -0600

    Use proxy for axios get request

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,5 +1,5 @@
-  import axios from "axios";
-  import {useState, useEffect} from "react"
+import {useState, useEffect} from "react"
+import axios from "axios";
   
   export default function useApplicationData() {
   //Combine all useStates into one
@@ -13,10 +13,10 @@
 
 useEffect(()=> {
   // Fetch days, appointments and interviewers data using API 
-  Promise.all([
-    axios.get("http://localhost:8001/api/days"),
-    axios.get("http://localhost:8001/api/appointments"),
-    axios.get("http://localhost:8001/api/interviewers")
+  Promise.all([ 
+    axios.get("/api/days"),
+    axios.get("/api/appointments"),
+    axios.get("/api/interviewers")
   ])
   .then((all) => {
     setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
