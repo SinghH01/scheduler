42fcd78805eb03cf6ef6f06d8e861e2508104ecb
Harshbir Singh <singh.hveer1@gmail.com>
2022-06-18 16:10:21 -0600

    Format code using prettier

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,41 +1,39 @@
-import React, {useEffect } from "react";
+import React, { useEffect } from "react";
 import "components/Application.scss";
 import Appointment from "./Appointment";
 import DayList from "./DayList";
 //import axios from "axios";
-import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
-import useApplicationData from "hooks/useApplicationData"
-
+import {
+  getAppointmentsForDay,
+  getInterview,
+  getInterviewersForDay,
+} from "helpers/selectors";
+import useApplicationData from "hooks/useApplicationData";
 
 export default function Application(props) {
-  const {
-    state,
-    setDay,
-    bookInterview,
-    cancelInterview
-  } = useApplicationData();
+  const { state, setDay, bookInterview, cancelInterview } =
+    useApplicationData();
 
   // Fetch appointments for individual day
   const appointments = getAppointmentsForDay(state, state.day);
-  
 
   const schedule = appointments.map((appointment) => {
     const interview = getInterview(state, appointment.interview);
-    const interviewers = getInterviewersForDay(state, state.day)
-  
+    const interviewers = getInterviewersForDay(state, state.day);
+
     return (
       <Appointment
         key={appointment.id}
         id={appointment.id}
         time={appointment.time}
         interview={interview}
-        interviewers = {interviewers}
-        bookInterview= {bookInterview}
-        cancelInterview ={cancelInterview}        
+        interviewers={interviewers}
+        bookInterview={bookInterview}
+        cancelInterview={cancelInterview}
       />
     );
-  }); 
-  
+  });
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -46,11 +44,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-        <DayList
-            days={state.days}
-            value={state.day}
-            onChange={setDay}
-          />
+          <DayList days={state.days} value={state.day} onChange={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
@@ -60,7 +54,7 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         {schedule}
-      <Appointment key="last" time="5pm" />
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
