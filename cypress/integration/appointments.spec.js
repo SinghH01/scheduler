80f1f7dbf0a502fb9e03245504dc276926e4c0bf
Harshbir Singh <singh.hveer1@gmail.com>
2022-06-20 02:03:51 -0600

    Add tests for Edit and deleting an appointment

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -22,14 +22,29 @@ describe("Appointments", () => {
   });
 
   it("should edit an interview", () => {
-    cy.contains("Archie Cohen");
- 
-  //  cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
-  //  cy.get('[alt="Sylvia Palmer"]').click();
- 
-  //  cy.contains("Save").click();
- 
-  //  cy.contains(".appointment__card--show", "Lydia Miller-Jones");
-  //  cy.contains(".appointment__card--show", "Sylvia Palmer");
+    cy.get("[alt=Edit]")
+      .first()
+      .click({ force: true });
+  
+    cy.get("[data-testid=student-name-input]").clear().type("Lydia Miller-Jones");
+    cy.get("[alt='Tori Malcolm']").click();
+  
+    cy.contains("Save").click();
+  
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+  });
+
+  it("should cancel an interview", () => {
+
+    cy.get("[alt=Delete]")
+    .first()
+    .click({ force: true });
+
+    cy.contains("Confirm").click();   
+    cy.contains("Deleting").should("exist");
+    cy.contains("Deleting").should("not.exist");
+    
+    cy.contains(".appointment__card--show", "Archie Cohen").should('not.exist');
   })
  });
