f94383181aa3cf35cbf2a4eafb2e59566258fd9a
Harshbir Singh <singh.hveer1@gmail.com>
2022-06-20 01:48:50 -0600

    Add test to book appointment

---
+++ cypress/integration/appointments.spec.js
@@ -0,0 +1,35 @@
+describe("Appointments", () => {
+  beforeEach(() => {
+   cy.request("GET", "/api/debug/reset");
+ 
+   cy.visit("/");
+ 
+   cy.contains("Monday");
+  });
+ 
+  it("should book an interview", () => {
+   cy.get("[alt=Add]")
+    .first()
+    .click();
+ 
+   cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
+   cy.get('[alt="Sylvia Palmer"]').click();
+ 
+   cy.contains("Save").click();
+ 
+   cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+   cy.contains(".appointment__card--show", "Sylvia Palmer");
+  });
+
+  it("should edit an interview", () => {
+    cy.contains("Archie Cohen");
+ 
+  //  cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
+  //  cy.get('[alt="Sylvia Palmer"]').click();
+ 
+  //  cy.contains("Save").click();
+ 
+  //  cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+  //  cy.contains(".appointment__card--show", "Sylvia Palmer");
+  })
+ });
