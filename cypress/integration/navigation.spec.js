e555839a2c6c4bb467836cb5660139f5b1f8b779
Harshbir Singh <singh.hveer1@gmail.com>
2022-06-20 02:05:40 -0600

    Format code

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -6,10 +6,8 @@ describe("Navigation", () => {
   it("should navigate to Tuesday", () => {
     cy.visit("/");
     cy.contains("[data-testid]", "Tuesday")
-    .click()
-    // Check if the selected element have class "day-list__item--selected"
-    .should("have.class", "day-list__item--selected");
-  })
-
+      .click()
+      // Check if the selected element have class "day-list__item--selected"
+      .should("have.class", "day-list__item--selected");
+  });
 });
-
